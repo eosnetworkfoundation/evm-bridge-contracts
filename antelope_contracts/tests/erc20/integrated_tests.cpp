625af97c65c940a01d397a6e6090f669557c6621
yarkin <yarkin.who@gmail.com>
2023-09-08 10:49:05 +0800

    More checkes to figure out why CI fails.

--- antelope_contracts/tests/erc20/integrated_tests.cpp
+++ antelope_contracts/tests/erc20/integrated_tests.cpp
@@ -105,7 +105,7 @@ struct it_tester : erc20_tester {
         exec_input input;
         input.context = context;
         input.to = *erc20_test::from_hex(evm_address.c_str());
-
+        BOOST_REQUIRE_MESSAGE(input.to.size() == 20, std::string("address wrong: ") + evm_address);
         bytes calldata;
         uint8_t func[4] = {0x6a, 0x03, 0x66, 0xbf};  // sha3(egressFee())[:4] = 6a0366bf
 
