ab3025e1ba4e571319fdf4e2ea61d09771b9f253
yarkin <yarkin.who@gmail.com>
2024-07-02 15:59:04 +0800

    Missing changes.

--- antelope_contracts/contracts/evmutil/include/evmutil/endrmng.hpp
+++ antelope_contracts/contracts/evmutil/include/evmutil/endrmng.hpp
@@ -10,11 +10,13 @@ namespace endrmng {
         public:
         void evmstake(const checksum256& proxy, const checksum256& staker, const name& validator, const uint64_t quantity); 
         void evmunstake(const checksum256& proxy, const checksum256& staker, const name& validator, const uint64_t quantity);   
-        void evmclaim(const checksum256& proxy, const checksum256& staker, const name& validator);  
+        void evmclaim(const checksum256& proxy, const checksum256& staker, const name& validator);
+        void evmnewstake(const checksum256& proxy, const checksum256& staker, const name& validator, const name& new_validator);
     };
 
     using evmstake_action = action_wrapper<"evmstake"_n, &contract_actions::evmstake>;
     using evmunstake_action = action_wrapper<"evmunstake"_n, &contract_actions::evmunstake>;
     using evmclaim_action = action_wrapper<"evmclaim"_n, &contract_actions::evmclaim>;
+    using evmnewstake_action = action_wrapper<"evmnewstake"_n, &contract_actions::evmnewstake>;
 
 }
